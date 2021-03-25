cce041540a811592a7381e82b9d7f9284d4d2141
Dani <dani>
2021-03-25 18:19:49 +0600

    changed business logic, thats why change migration file

--- src/main/java/com/attractor/onlineshop/config/SecurityConfig.java
+++ src/main/java/com/attractor/onlineshop/config/SecurityConfig.java
@@ -1,17 +1,15 @@
 package com.attractor.onlineshop.config;
 
-import com.attractor.onlineshop.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -38,14 +36,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
               .csrf().disable();
+//        .cors().disable();
         http
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET,"/shop/**")
+                .permitAll()
+                .antMatchers("/orders/shop")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .oauth2ResourceServer();
     }
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+//        return source;
+//    }
 }
 
