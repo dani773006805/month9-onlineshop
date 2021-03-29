bb551b3b75de07b36d181425455b37e3ae0c272c
Dani <dani>
2021-03-29 16:18:14 +0600

    add security config

--- src/main/java/com/attractor/onlineshop/config/SecurityConfig.java
+++ src/main/java/com/attractor/onlineshop/config/SecurityConfig.java
@@ -1,47 +1,29 @@
 package com.attractor.onlineshop.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    private UserService userService;
-//
-//    @Autowired
-//    public void setUserService(UserService userService) {
-//        this.userService = userService;
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userService);
-//    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-              .csrf().disable();
-//        .cors().disable();
+                .csrf().disable();
         http
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET,"/shop/**")
+                .antMatchers(HttpMethod.GET, "/shop/**")
                 .permitAll()
                 .antMatchers("/orders/shop")
+                .permitAll()
+                .antMatchers(HttpMethod.GET, "/reviews/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
